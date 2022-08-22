ae0a5e57db003e1fae61c2b8a986219dd8bb51ff
NelsonKhor <cnkhor92@gmail.com>
2022-08-22 11:51:56 +0800

    Add basic calculator functions

---
+++ script.js
@@ -0,0 +1,25 @@
+// Add function
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// Subtract function
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+// Multiply function
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// Divide function
+function divide(num1, num2) {
+    return num1/num2;
+}
+
+// // Test Cases
+// console.log(add(5,2));
+// console.log(subtract(5,2));
+// console.log(multiply(5,2));
+// console.log(divide(7,3));
