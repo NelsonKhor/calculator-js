ebde01a1cf84ced0885358511080c48770dbb3b6
NelsonKhor <cnkhor92@gmail.com>
2022-08-24 10:16:25 +0800

    Link button value to currentDisplay on click

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// DOM Selections
+const buttons = document.querySelectorAll('button');
+const currentDisplay = document.querySelector('.currentDisplay');
+const pastDisplay = document.querySelector('.pastDisplay');
+const allClearButton = document.querySelector('.clearButton');
+const equalButton = document.querySelector('.equal');
+const deleteButton = document.getElementById('delete');
+
+// Get Buttons' value
+buttons.forEach(button => button.addEventListener('click', () => {
+    const displayValue = button.getAttribute('value');
+    currentDisplay.textContent = displayValue;
+    console.log(button.getAttribute('value'));
+    return button.getAttribute('value');
+}));
+
 // Add function
 function add(num1, num2) {
     return num1 + num2;
@@ -34,5 +50,3 @@ function operate(operator, num1, num2) {
     }
 }
 
-// Test Cases
-// console.log(operate('+','r',3));
