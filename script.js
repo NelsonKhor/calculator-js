b6da9e5ff9fbeb1fcf14584d9b4b785fdc8fb7ca
NelsonKhor <cnkhor92@gmail.com>
2022-08-24 12:40:17 +0800

    Make + - * / work but =

--- script.js
+++ script.js
@@ -1,26 +1,74 @@
 // DOM Selections
 const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
 const currentDisplay = document.querySelector('.currentDisplay');
 const pastDisplay = document.querySelector('.pastDisplay');
 const allClearButton = document.querySelector('.clearButton');
-const equalButton = document.querySelector('.equal');
+const equalButton = document.getElementById('equal');
 const deleteButton = document.getElementById('delete');
 const dotButton = document.getElementById('dot')
 
 // Initialize Values
 currentDisplay.textContent = "";
 pastDisplay.textContent = "";
-let firstNumber = 0;
-let operator;
-let secondNumber = 0;
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let result = null;
 
-// Get Buttons' value
-numberButtons.forEach(numButton => numButton.addEventListener('click', () => {
-    const displayValue = numButton.getAttribute('value');
+// Add 'click' event listener to buttons 
+numberButtons.forEach(numButton => numButton.addEventListener('click', getNumber));
+operatorButtons.forEach(opButton => opButton.addEventListener('click', getOperator));
+// equalButton.addEventListener('click', inputEqual);
+
+// Function: Get number buttons' value
+function getNumber(e){
+    const displayValue = e.target.value;
     currentDisplay.textContent += displayValue;
-    console.log(numButton.getAttribute('value'));
-    return numButton.getAttribute('value');
-}));
+}
+
+// Function: Get operator buttons' value
+function getOperator(e){
+    // check if the first entered value existed
+    if (currentDisplay.textContent == "" && firstNumber == nul){
+        alert("Enter the first number first!");
+        return; // stop the function
+    }
+    if (firstNumber != null) {
+        inputSecondNum(e);
+        return;
+    }
+    // get operator value
+    operator = e.target.value;
+    // get first number value from current display
+    firstNumber = parseInt(currentDisplay.textContent);
+    // add first number value and operator to past display
+    pastDisplay.textContent = currentDisplay.textContent + operator;
+    // clear current display
+    currentDisplay.textContent = "";
+}
+
+// Function: Input second number
+function inputSecondNum(e){
+    secondNumber = parseInt(currentDisplay.textContent);
+    result = operate(operator,firstNumber,secondNumber);
+    pastDisplay.textContent = result + e.target.value;
+    operator = e.target.value;
+    firstNumber = result;
+    currentDisplay.textContent = "";
+    console.log(`${firstNumber}${operator}${secondNumber}${e.target.value}`);
+}
+
+// Function: Equal sign operation
+// function inputEqual(e){
+//     console.log(`${firstNumber}${operator}${secondNumber}${e.target.value}`);
+//     secondNumber = parseInt(currentDisplay.textContent);
+//     result = operate(operator,firstNumber,secondNumber);
+//     pastDisplay.textContent = result;
+//     firstNumber = result;
+//     currentDisplay.textContent = "";
+//     operator = null;
+// }
 
 // Add function
 function add(num1, num2) {
@@ -45,13 +93,13 @@ function divide(num1, num2) {
 // Operate function
 function operate(operator, num1, num2) {
     switch(true) {
-        case operator == "+":
+        case operator === "+":
             return add(num1, num2);
-        case operator == "-":
+        case operator === "-":
             return subtract(num1, num2);
-        case operator == "*":
+        case operator === "*":
             return multiply(num1, num2);
-        case operator == "/":
+        case operator === "/":
             return divide(num1, num2);
         default:
             console.log("operate() ERROR")
